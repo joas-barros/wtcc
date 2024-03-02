c0d6d8a8a4eacfc55444cdb123ed77950308f5cd
joas-barros <joas.barros14@hotmail.com>
2024-03-02 08:33:13 -0300

    modify pictures to joas version

--- src/components/Pictures/Pictures.tsx
+++ src/components/Pictures/Pictures.tsx
@@ -1,112 +1,246 @@
-import React, { useState, useEffect } from 'react';
-import { Pagination, Image } from 'antd';
-import i1 from '../../assets/gallery/1.jpeg';
-import i2 from '../../assets/gallery/2.jpeg';
-import i3 from '../../assets/gallery/3.jpeg';
-import i4 from '../../assets/gallery/4.jpeg';
-import i5 from '../../assets/gallery/5.jpeg';
-import i6 from '../../assets/gallery/6.jpeg';
-import i7 from '../../assets/gallery/7.jpeg';
-import i8 from '../../assets/gallery/8.jpeg';
-import i9 from '../../assets/gallery/9.jpeg';
-import i10 from '../../assets/gallery/10.jpeg';
-import i11 from '../../assets/gallery/11.jpeg';
-import i12 from '../../assets/gallery/12.jpeg';
-import i19 from '../../assets/gallery/19.jpg';
-import i35 from '../../assets/gallery/35.jpg';
-import i36 from '../../assets/gallery/36.jpg';
-import i38 from '../../assets/gallery/38.jpg';
-import i39 from '../../assets/gallery/39.jpg';
-import i40 from '../../assets/gallery/40.jpg';
-import i41 from '../../assets/gallery/41.jpg';
-import i42 from '../../assets/gallery/42.jpg';
+import React from 'react';
+import { useState } from "react";
+import { Pagination } from 'antd';
+import { Image } from 'antd';
 
-//TODO add descriptions to the images
 
-const imageFiles = [
-    { link: i1, description: "" },
-    { link: i2, description: "" },
-    { link: i3, description: "" },
-    { link: i4, description: "" },
-    { link: i5, description: "" },
-    { link: i6, description: "" },
-    { link: i7, description: "" },
-    { link: i8, description: "" },
-    { link: i9, description: "" },
-    { link: i10, description: "" },
-    { link: i11, description: "" },
-    { link: i12, description: "" },
-    { link: i19, description: "" },
-    { link: i35, description: "" },
-    { link: i36, description: "" },
-    { link: i38, description: "" },
-    { link: i39, description: "" },
-    { link: i40, description: "" },
-    { link: i41, description: "" },
-    { link: i42, description: "" }
+interface PictureProps {
+    scr: string;
+    alt: string;
+    subtitle: string;
+    page: number;
+    }
+
+
+const pictures = [
+    {
+        scr: require("../../assets/pictures/01/nepal1.jpg"),
+        alt: "Nepal1",
+        subtitle: "Nepal",
+        page: 1
+    },
+    {
+        scr: require("../../assets/pictures/01/nepal2.jpg"),
+        alt: "Nepal2",
+        subtitle: "Nepal",
+        page: 1
+    },
+    {
+        scr: require("../../assets/pictures/01/nepal3.jpg"),
+        alt: "Nepal3",
+        subtitle: "Nepal",
+        page: 1
+    },
+    {
+        scr: require("../../assets/pictures/01/nepal4.jpg"),
+        alt: "Nepal4",
+        subtitle: "Nepal",
+        page: 1
+    },
+    {
+        scr: require("../../assets/pictures/01/nepal5.jpg"),
+        alt: "Nepal5",
+        subtitle: "Nepal",
+        page: 1
+    },
+    {
+        scr: require("../../assets/pictures/01/nepal6.jpg"),
+        alt: "Nepal6",
+        subtitle: "Nepal",
+        page: 1
+    },
+    {
+        scr: require("../../assets/pictures/02/bolivia1.png"),
+        alt: "Bolivia1",
+        subtitle: "Bolivia",
+        page: 2
+    },
+    {
+        scr: require("../../assets/pictures/02/bolivia2.png"),
+        alt: "Bolivia2",
+        subtitle: "Bolivia",
+        page: 2
+    },
+    {
+        scr: require("../../assets/pictures/02/bolivia3.png"),
+        alt: "Bolivia3",
+        subtitle: "Bolivia",
+        page: 2
+    },
+    {
+        scr: require("../../assets/pictures/02/bolivia4.png"),
+        alt: "Bolivia4",
+        subtitle: "Bolivia",
+        page: 2
+    },
+    {
+        scr: require("../../assets/pictures/02/bolivia5.png"),
+        alt: "Bolivia5",
+        subtitle: "Bolivia",
+        page: 2
+    },
+    {
+        scr: require("../../assets/pictures/02/bolivia6.png"),
+        alt: "Bolivia6",
+        subtitle: "Bolivia",
+        page: 2
+    },
+    {
+        scr: require("../../assets/pictures/03/guatemala1.png"),
+        alt: "Gualtemala1",
+        subtitle: "Guatemala",
+        page: 3
+    },
+    {
+        scr: require("../../assets/pictures/03/guatemala2.png"),
+        alt: "Gualtemala2",
+        subtitle: "Guatemala",
+        page: 3
+    },
+    {
+        scr: require("../../assets/pictures/03/guatemala3.png"),
+        alt: "Gualtemala3",
+        subtitle: "Guatemala",
+        page: 3
+    },
+    {
+        scr: require("../../assets/pictures/03/guatemala4.png"),
+        alt: "Gualtemala4",
+        subtitle: "Guatemala",
+        page: 3
+    },
+    {
+        scr: require("../../assets/pictures/03/guatemala5.png"),
+        alt: "Gualtemala5",
+        subtitle: "Guatemala",
+        page: 3
+    },
+    {
+        scr: require("../../assets/pictures/03/guatemala6.png"),
+        alt: "Gualtemala6",
+        subtitle: "Guatemala",
+        page: 3
+    },
+    {
+        scr: require("../../assets/pictures/04/Vietnam1.png"),
+        alt: "Vietnam1",
+        subtitle: "Vietnam",
+        page: 4
+    },
+    {
+        scr: require("../../assets/pictures/04/Vietnam2.png"),
+        alt: "Vietnam2",
+        subtitle: "Vietnam",
+        page: 4
+    },
+    {
+        scr: require("../../assets/pictures/04/Vietnam3.png"),
+        alt: "Vietnam3",
+        subtitle: "Vietnam",
+        page: 4
+    },
+    {
+        scr: require("../../assets/pictures/04/Vietnam4.png"),
+        alt: "Vietnam4",
+        subtitle: "Vietnam",
+        page: 4
+    },
+    {
+        scr: require("../../assets/pictures/04/Vietnam5.png"),
+        alt: "Vietnam5",
+        subtitle: "Vietnam",
+        page: 4
+    },
+    {
+        scr: require("../../assets/pictures/04/Vietnam6.png"),
+        alt: "Vietnam6",
+        subtitle: "Vietnam",
+        page: 4
+    },
+    {
+        scr: require("../../assets/pictures/05/Argentina1.png"),
+        alt: "Argentina1",
+        subtitle: "Argentina",
+        page: 5
+    },
+    {
+        scr: require("../../assets/pictures/05/Argentina2.png"),
+        alt: "Argentina2",
+        subtitle: "Argentina",
+        page: 5
+    },
+    {
+        scr: require("../../assets/pictures/05/Argentina3.png"),
+        alt: "Argentina3",
+        subtitle: "Argentina",
+        page: 5
+    },
+    {
+        scr: require("../../assets/pictures/05/Argentina4.png"),
+        alt: "Argentina4",
+        subtitle: "Argentina",
+        page: 5
+    },
+    {
+        scr: require("../../assets/pictures/05/Argentina5.png"),
+        alt: "Argentina5",
+        subtitle: "Argentina",
+        page: 5
+    },
+    {
+        scr: require("../../assets/pictures/05/Argentina6.png"),
+        alt: "Argentina6",
+        subtitle: "Argentina",
+        page: 5
+    }
 ];
 
 const Pictures = () => {
-
-    //will be used to trigger a re-render
-    const [key, setKey] = useState(0);
-
     const [currentPage, setCurrentPage] = useState(1);
-    const imagesPerPage = 6;
-
-    // calculate the images for the current page
-    const indexOfLastImage = currentPage * imagesPerPage;
-    const indexOfFirstImage = indexOfLastImage - imagesPerPage;
-    const currentImages = imageFiles.slice(indexOfFirstImage, indexOfLastImage);
-
+    const imagePerPaage = 6;
+    const totalImages = pictures.length;
+    const totalPages = Math.ceil(totalImages / imagePerPaage);
+    
     const handlePageChange = (page: number) => {
         setCurrentPage(page);
-        setKey(prevKey => prevKey + 1);
-    };
+    }
 
-    currentImages.map((image, index) => {
-        if (image.description === "") {
-            image.description = "Imagem de eventos passados";
-        }
-        return image;
-    });
+    const renderPictures = () => {
+        const start = (currentPage - 1) * imagePerPaage;
+        const end = start + imagePerPaage;
+        const currentPictures = pictures.slice(start, end);
 
-    // calculate the number of empty divs required
-    const emptyDivsCount = imagesPerPage - currentImages.length;
+        return currentPictures.map((picture, index) => {
+            return (
+                <div key={index} className="flex-col">
+                    <Image
+                        width={400}
+                        height={250}
+                        src={picture.scr}
+                        alt={picture.alt}
+                        style={{ borderRadius: "1.5rem" }}
+                    />
+                    <div>{picture.subtitle}</div>
+                </div>
+            );
+        });
+    }
 
     return (
-        <div className="pt-36 mb-20 flex flex-col flex-wrap items-center" id="Galeria">
-            <h2 className="bg-yellow-400 text-4xl p-1 rounded-lg mb-12">GALERIA</h2>
-            <div className="grid grid-cols-12 gap-8 mx-36">
-                {currentImages.map((image, index) => (
-                    <div key={index} className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-4 xl:col-span-4">
-                        <Image
-                            key={index + key} // add key here
-                            src={image.link}
-                            alt="Imagem de eventos passados"
-                            className="w-200 h-100 rounded-lg image-fade-in"
-                        />
-                        <p className="ml-6 text-left text-sm">{image.description}</p>
-                    </div>
-                ))}
-                {/* render the empty divs */}
-                {Array.from({ length: emptyDivsCount }, (_, index) => (
-                    <div
-                        key={index + currentImages.length}
-                        className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-4 xl:col-span-4 bg-gray-200 rounded-lg"
-                    />
-                ))}
+        <div className="flex items-center flex-col gap-10 pt-36">
+            <h1 className="w-48 text-4xl bg-yellow-300 uppercase rounded-lg px-1 mb-2 ">Galeria</h1>
+            <div className="flex justify-center gap-10 flex-wrap">
+                <Image.PreviewGroup>
+                    {renderPictures()}
+                </Image.PreviewGroup>
             </div>
-            <div className="mt-5">
-                <Pagination
-                    defaultCurrent={1}
-                    total={imageFiles.length}
-                    pageSize={imagesPerPage}
-                    onChange={handlePageChange}
-                />
-            </div>
+            <Pagination 
+                current={currentPage}
+                total={totalPages * 10}
+                onChange={handlePageChange}
+            />
         </div>
     );
-}
+};
 
 export default Pictures;
